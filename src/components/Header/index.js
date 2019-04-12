4e2e1191fa4eee26802d72b83a08b0692dd35284
mikspinzulis <miks.pinzulis@gmail.com>
2019-04-12 11:36:31 +0300

    RT-05: New navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import palms from '../../images/palms.jpg';
 import { HeaderWrapper } from './HeaderStyle'
 
-import { StickyContainer } from 'react-sticky';
+import Sticky from 'react-sticky-el';
+import styled from "styled-components";
+
+const Navigation = styled(Sticky)`
+  position: relative;
+  z-index: 100;
+  ul {
+    background-color: transparent;
+    display: flex;
+    justify-content: center;
+  }
+  &.sticky {
+    ul {
+      background-color: black;
+    }
+  }
+`
 
 class Header extends Component {
   render() {
     return (
       <HeaderWrapper>
-        <StickyContainer>
+        <Navigation>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">Gallery</a></li>
@@ -16,7 +32,7 @@ class Header extends Component {
             <li><a href="#">About</a></li>
             <li><a href="#">Blog</a></li>
           </ul>
-        </StickyContainer>
+        </Navigation>
         <img src={palms} className="palms"/>
       </HeaderWrapper>
     );
